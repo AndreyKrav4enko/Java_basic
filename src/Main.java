5968b98856c3be0e2946d6bfe0554261f0f93a0c
Andrey <kb196905@gmail.com>
2022-01-26 20:55:08 +0300

    Добавлена задача №2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //Присваиваем значения переменным
+        double x = 234.3434;
+        byte y = 127;
     }
+
+
 }
