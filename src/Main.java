f9a9e285b9f15e0051af091605ce31786e06fdfd
Andrey <kb196905@gmail.com>
2022-01-26 23:11:42 +0300

    Добавлена задача №3

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ public class Main {
         //Присваиваем значения переменным
         double x = 234.3434;
         byte y = 127;
+
+        //Задача №3
+        int[] array = {1, 8, 22, 15, 50};
+        array[0] = array[array.length - 1];
+        array[4] = 1;
+        System.out.println(array[0] + array[array.length - 3]);
+
     }
-
-
 }
