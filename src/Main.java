ae15ef1ce8612531f6d26c17a2c0bcecd388a803
Andrey <kb196905@gmail.com>
2022-02-03 13:51:49 +0300

    Добавлена задача №4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -11,5 +13,23 @@ public class Main {
         array[4] = 1;
         System.out.println(array[0] + array[array.length - 3]);
 
+        //Задача №4
+        //1
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите числа:");
+        double num1 = in.nextInt();
+        double num2 = in.nextInt();
+        double num3 = in.nextInt();
+        //2
+        double num4 = (num1 + num2 + num3) / 3;
+        System.out.printf("Среднее арифметическое чисел равно: %.0f \n", num4);
+        //3
+        double round = Math.floor(num4/2);
+        if (round>3){
+            System.out.println("Программа выполнена корректно");
+        }
+        else {
+            System.out.println("Что-то пошло не так");
+        }
     }
 }
